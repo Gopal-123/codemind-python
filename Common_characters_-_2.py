83bcd62658987e5ca2a2f88248c75b111cd21190
kanuboina Gopala krishna <98656499+Gopal-123@users.noreply.github.com>
2022-12-11 13:29:02 +0530

    a=input() a=a.lower() b=a.split() d=b[0] s=[] for i in d:     c=0     for j in range(1,len(b)):         if i in b[j]:            c=c+1     if c==len(b)-1:         s.append(i) print(len(s))

---
+++ Common_characters_-_2.py
@@ -0,0 +1,15 @@
+a=input()
+a=a.lower()
+b=a.split()
+d=b[0]
+s=[]
+for i in d:
+    c=0
+    for j in range(1,len(b)):
+        if i in b[j]:
+           c=c+1
+    if c==len(b)-1:
+        s.append(i)
+print(len(s))
+    
+    
