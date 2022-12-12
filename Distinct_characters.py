f49eb7a74cdd4264436510c1054b5b014910eb72
kanuboina Gopala krishna <98656499+Gopal-123@users.noreply.github.com>
2022-12-12 08:22:07 +0530

    n=input() n=n.lower() u=[] for i in n:     if ord(i)>=97 and ord(i)<=122 and i not in u:         u.append(i) u.sort() f="".join(u) print(f)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,13 +1,9 @@
 n=input()
+n=n.lower()
 u=[]
 for i in n:
-    if ord(i)>=65 and ord(i)<=90:
-        if n.count(i)==1 and chr(ord(i)+32) not in n:
-            u.append(i)
-    elif ord(i)>=95 and ord(i)<=122:
-        if n.count(i)==1 and chr(ord(i)-32) not in n:
-            u.append(i)
-u.sort()
-b="".join(u)
-print(b)
-        
+    if ord(i)>=97 and ord(i)<=122 and i not in u:
+        u.append(i)
+u.sort()  
+f="".join(u)
+print(f)
