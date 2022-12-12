99265636340f3eff9ed5d6dbfbb5d817a12de132
kanuboina Gopala krishna <98656499+Gopal-123@users.noreply.github.com>
2022-12-12 14:48:53 +0530

    n=input() l=list(map(int,input().split())) e=[] o=[] for i in range(len(l)):     if i%2==0:         e.append(l[i])     elif i%2:         o.append(l[i]) if sum(e)-sum(o)==0:     print("YES") else:     print("NO")

---
+++ Sum_of_odd_and_even_digits.py
@@ -0,0 +1,13 @@
+n=input()
+l=list(map(int,input().split()))
+e=[]
+o=[]
+for i in range(len(l)):
+    if i%2==0:
+        e.append(l[i])
+    elif i%2:
+        o.append(l[i])
+if sum(e)-sum(o)==0:
+    print("YES")
+else:
+    print("NO")
